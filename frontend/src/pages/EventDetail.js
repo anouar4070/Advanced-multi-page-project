f3d5665e0b1d04ca9679b5f90993eed506ef308e
anouar garbaa <g.anouar@yahoo.com>
2024-10-30 12:38:55 +0100

    add EventsRootLayout as a nested layout route

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,11 +1,18 @@
+import { useParams } from "react-router-dom";
+
+function EventDetailPage() {
+  const params = useParams();
 
 
- function EventDetailPage() {
-  return (
-    <div>
-      EventDetailPage
-    </div>
+  return(
+    <>
+     <h1>EventDetailPage</h1>
+     <p>Event ID: {params.eventId}</p>
+    </>
   )
+  
 }
 
 export default EventDetailPage;
+
+//eventId must match the parameter name you've defined in your route path, it must be :eventId is the dynamic part of the URL
