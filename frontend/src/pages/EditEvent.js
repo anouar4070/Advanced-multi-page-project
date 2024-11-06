d8c0e7e352bc1c07e1ec65bced9430bbb3608f1a
anouar garbaa <g.anouar@yahoo.com>
2024-11-06 15:57:00 +0100

    useRouteLoaderData Hook to access to a higher level loader

--- frontend/src/pages/EditEvent.js
+++ frontend/src/pages/EditEvent.js
@@ -1,6 +1,11 @@
+import { useRouteLoaderData } from "react-router-dom";
+import EventForm from "../components/EventForm";
+
  function EditEventPage() {
-  
-  return <div>EditEventPage</div>;
+  const data = useRouteLoaderData('event-detail');
+
+
+  return <EventForm event={data.event} /> ;
 }
 
 export default EditEventPage;
